1da4b92d9aedf276cc9d21dabd70e58e64e5967b
andresfzv-dev <264909460+andresfzv-dev@users.noreply.github.com>
2026-03-28 19:44:13 -0500

    Base inicial del proyecto

---
+++ animascotas-backend/src/main/java/com/animascotas/animascotas_backend/AnimascotasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.animascotas.animascotas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimascotasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimascotasBackendApplication.class, args);
+	}
+
+}
---
+++ animascotas-backend/src/test/java/com/animascotas/animascotas_backend/AnimascotasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animascotas.animascotas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimascotasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
